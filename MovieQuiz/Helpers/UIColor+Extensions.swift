d57521d625d8b79494fbaa338a6c678d85fff5ff
GolovkinEgor <119954022+GolovkinEgor@users.noreply.github.com>
2024-09-17 14:40:24 +0300

    исправлен шрифт и решена проблема с run time

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,13 +2,8 @@ import UIKit
 
 extension UIColor {
          static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
-<<<<<<< HEAD
          static var YpGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
          static var YpRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
-=======
-         static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
-         static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
->>>>>>> c2df88807df59aaa3110dda697341eef26e84172
          static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
          static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
          static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
